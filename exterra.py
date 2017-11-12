d9b30c142a2f5c95e186e08bf179135b677e2d17
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2017-11-12 20:26:44 +0000

    We can have pngs again!

--- exterra.py
+++ exterra.py
@@ -30,7 +30,6 @@ def main():
     some_data_plot = linear_plot([3, 1, 2, 7], size_in_inches = [3, 3]) #plot the points, optional arguments after
     images["linegraph"] = graph_image(some_data_plot) #generate an image pygame understands
     images["piechart"] = graph_image(pie_chart([3.14159, 6.28318], labels = ["pi", "tau"], explode = [0.1, 0], shadow = True, size_in_inches = [3.14, 3.14]))
-    save_graph(some_data_plot) #gets saved to a .png tagged with the current date & time
 
 
     #
@@ -53,7 +52,7 @@ def main():
     #
     save = Save()
     save.load()
-    
+
 
     #checking save works
     print(save.__dict__) #print all of its members
